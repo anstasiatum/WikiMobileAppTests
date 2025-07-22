694563ae6e7dd3724ed5d418b4c2892ab194a57a
anstasiatum <anastasiya.tumakowa@gmail.com>
2025-07-22 17:02:59 +0300

    added BrowserstackConfig

--- src/test/java/twentiethhometask/config/BrowserstackConfig.java
+++ src/test/java/twentiethhometask/config/BrowserstackConfig.java
@@ -9,38 +9,29 @@ import org.aeonbits.owner.Config;
 public interface BrowserstackConfig extends Config {
 
     @Key("browserstack.userName")
-    @DefaultValue("anastasiyatumako_1bZRWy")
     String getUserName();
 
     @Key("browserstack.accessKey")
-    @DefaultValue("VUZLPg4b4BKfxYSKVfFi")
     String getAccessKey();
 
     @Key("browserstack.app")
-    @DefaultValue("bs://sample.app")
     String getApp();
 
     @Key("browserstack.device")
-    @DefaultValue("Google Pixel 3")
     String getDevice();
 
     @Key("browserstack.osValue")
-    @DefaultValue("9.0")
     String getOSValue();
 
     @Key("browserstack.projectName")
-    @DefaultValue("Wikipedia Mobile App Test")
     String getProjectName();
 
     @Key("browserstack.buildName")
-    @DefaultValue("browserstack-build")
     String getBuildName();
 
     @Key("browserstack.sessionName")
-    @DefaultValue("first_test")
     String getSessionName();
 
     @Key("browserstack.hubURL")
-    @DefaultValue("https://hub.browserstack.com/wd/hub")
     String getHubURL();
 }
