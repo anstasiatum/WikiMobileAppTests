3e62f1e7b5d9fe2ffd4ec22919f33a328e7fb9cd
anstasiatum <anastasiya.tumakowa@gmail.com>
2025-07-22 17:10:53 +0300

    added BrowserstackConfig

--- src/test/java/twentiethhometask/config/BrowserstackConfig.java
+++ src/test/java/twentiethhometask/config/BrowserstackConfig.java
@@ -2,7 +2,9 @@ package twentiethhometask.config;
 
 import org.aeonbits.owner.Config;
 
+@Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
+        "system:properties",
         "classpath:${platform}.properties"
 })
 
